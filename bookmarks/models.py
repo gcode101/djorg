e1c67fbdf1e1ba23e1e726275e87b4091db0ee30
Guelmis Cortina <cguelmis@yahoo.com>
2018-05-11 14:10:12 -0400

    add PersonalBookmark model

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Bookmark(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -8,3 +9,6 @@ class Bookmark(models.Model):
 	notes = models.TextField(blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBookmark(Bookmark):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
