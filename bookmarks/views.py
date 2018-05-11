585480330362f53576b1aa2a65c8ac553d17fc17
Guelmis Cortina <cguelmis@yahoo.com>
2018-05-11 14:27:56 -0400

    Expose personal bookmarks in template/view for logged in user

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 def index(request):
 	context = {}
 	context['bookmarks'] = Bookmark.objects.all()
+	if request.user.is_anonymous:
+		context['personal_bookmarks'] = PersonalBookmark.objects.none()
+	else:
+		context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
 	return render(request, 'bookmarks/index.html', context)
