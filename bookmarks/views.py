54fa43a73a26be7570713e5fde42aa3bc5429214
Guelmis Cortina <cguelmis@yahoo.com>
2018-05-11 14:35:28 -0400

    exclude personal bookmarks from public list

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -3,7 +3,7 @@ from .models import Bookmark, PersonalBookmark
 
 def index(request):
 	context = {}
-	context['bookmarks'] = Bookmark.objects.all()
+	context['bookmarks'] = Bookmark.objects.exclude(id__in=PersonalBookmark.objects.values_list('id'))
 	if request.user.is_anonymous:
 		context['personal_bookmarks'] = PersonalBookmark.objects.none()
 	else:
