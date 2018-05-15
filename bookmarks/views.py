5fd798ed81c17c80e2c76cc3e582ee977200308f
Guelmis Cortina <cguelmis@yahoo.com>
2018-05-14 21:45:20 -0400

    add bookmark form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .forms import BookmarkForm
 from .models import Bookmark, PersonalBookmark
 
 def index(request):
+	# break point used for debuging
+	# import pdb; pdb.set_trace()
+	if request.method == 'POST':
+		form = BookmarkForm(request.POST)
+		if form.is_valid():
+			form.save()
 	context = {}
 	context['bookmarks'] = Bookmark.objects.exclude(id__in=PersonalBookmark.objects.values_list('id'))
 	if request.user.is_anonymous:
 		context['personal_bookmarks'] = PersonalBookmark.objects.none()
 	else:
 		context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
+	context['form'] = BookmarkForm()
 	return render(request, 'bookmarks/index.html', context)
